bc635676ebd3186faa90258a9ce2f19bc8ee8777
Marvin Leiers <marvin@leiers.org>
2023-05-05 19:48:06 +0200

    Initial commit

---
+++ src/main/java/net/mplugins/bindkeys/BindKeysPlugin.java
@@ -0,0 +1,12 @@
+package net.mplugins.bindkeys;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BindKeysPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
